1d6ed4d54f2a58978d5944242913c188fa4e43d5
Jeff Rasley <jerasley@microsoft.com>
2020-02-19 20:08:47 +0000

    add unit tests for non-zero loss scale value

--- tests/unit/test_fp16.py
+++ tests/unit/test_fp16.py
@@ -206,3 +206,80 @@ def test_adamw_fp16_empty_grad(tmpdir):
             model.step()
 
     _test_adamw_fp16_empty_grad(args=args, model=model, hidden_dim=hidden_dim)
+
+
+def test_adam_fp16_non0_loss_scale(tmpdir):
+    config_dict = {
+        "train_batch_size": 4,
+        "steps_per_print": 1,
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+                "lr": 0.00015,
+            }
+        },
+        "fp16": {
+            "enabled": True,
+            "loss_scale": 10
+        }
+    }
+    args = get_args(tmpdir, config_dict)
+    hidden_dim = 10
+
+    model = SimpleModel(hidden_dim, empty_grad=True)
+
+    @distributed_test(world_size=[1])
+    def _test_adam_fp16_non0_loss_scale(args, model, hidden_dim):
+        model, _, _,_ = deepspeed.initialize(args=args,
+                                             model=model,
+                                             model_parameters=model.parameters(),
+                                             dist_init_required=False)
+        data_loader = get_data_loader(model=model,
+                                      total_samples=50,
+                                      hidden_dim=hidden_dim,
+                                      device=model.device)
+        for n, batch in enumerate(data_loader):
+            loss = model(batch[0], batch[1])
+            model.backward(loss)
+            model.step()
+
+    _test_adam_fp16_non0_loss_scale(args=args, model=model, hidden_dim=hidden_dim)
+
+
+def test_adam_fp16_zero_non0_loss_scale(tmpdir):
+    config_dict = {
+        "train_batch_size": 4,
+        "steps_per_print": 1,
+        "optimizer": {
+            "type": "Adam",
+            "params": {
+                "lr": 0.00015,
+            }
+        },
+        "fp16": {
+            "enabled": True,
+            "loss_scale": 10
+        },
+        "zero_optimization": True
+    }
+    args = get_args(tmpdir, config_dict)
+    hidden_dim = 10
+
+    model = SimpleModel(hidden_dim, empty_grad=True)
+
+    @distributed_test(world_size=[1])
+    def _test_adam_fp16_zero_non0_loss_scale(args, model, hidden_dim):
+        model, _, _,_ = deepspeed.initialize(args=args,
+                                             model=model,
+                                             model_parameters=model.parameters(),
+                                             dist_init_required=False)
+        data_loader = get_data_loader(model=model,
+                                      total_samples=50,
+                                      hidden_dim=hidden_dim,
+                                      device=model.device)
+        for n, batch in enumerate(data_loader):
+            loss = model(batch[0], batch[1])
+            model.backward(loss)
+            model.step()
+
+    _test_adam_fp16_zero_non0_loss_scale(args=args, model=model, hidden_dim=hidden_dim)
